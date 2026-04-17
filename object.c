a6f92c11ac8347642cbbd61e584c902ac3aea611
udayeswar sreerama <udayeswar2002@gmail.com>
2026-04-17 19:23:13 +0530

    Add SHA-256 hash generation for objects

--- object.c
+++ object.c
@@ -3,7 +3,7 @@
 // Every piece of data (file contents, directory listings, commits) is stored
 // as an "object" named by its SHA-256 hash. Objects are stored under
 // .pes/objects/XX/YYYYYYYY... where XX is the first two hex characters of the
-// hash (directory sharding).
+// hash (directory sharding)
 //
 // PROVIDED functions: compute_hash, object_path, object_exists, hash_to_hex, hex_to_hash
 // TODO functions: object_write, object_read
